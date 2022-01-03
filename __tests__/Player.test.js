750f9e769cb9a4910ee4c3179516f24661b9787c
Jennifer Emanuele <jemanuele@gmail.com>
2022-01-03 11:27:30 -0500

    potion & player testing done

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,24 @@
-const { jest } = require('@jest/globals');
+const { expect } = require('@jest/globals');
 const Player = require('../lib/Player');
 
 jest.mock('../lib/Potion');
+
+test('creates a player object', () => {
+    const player = new Player('Dave');
+  
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(object)])
+    );
+  });
+
+//   const Potion = require('../lib/Potion');
+
+//   jest.mock('../lib/Potion.js');
+  
+//   console.log(new Potion());
+  
